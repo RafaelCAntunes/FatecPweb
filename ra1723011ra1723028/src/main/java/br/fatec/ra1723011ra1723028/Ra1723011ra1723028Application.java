b5acaf82fa7ccc0efc30040034edf5d2884a3b47
RafaelCAntunes <30886398+RafaelCAntunes@users.noreply.github.com>
2019-11-30 14:10:53 -0300

    Add files via upload
    Create Repo

---
+++ ra1723011ra1723028/src/main/java/br/fatec/ra1723011ra1723028/Ra1723011ra1723028Application.java
@@ -0,0 +1,13 @@
+package br.fatec.ra1723011ra1723028;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ra1723011ra1723028Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ra1723011ra1723028Application.class, args);
+	}
+
+}
---
+++ ra1723011ra1723028/src/test/java/br/fatec/ra1723011ra1723028/Ra1723011ra1723028ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fatec.ra1723011ra1723028;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ra1723011ra1723028ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
